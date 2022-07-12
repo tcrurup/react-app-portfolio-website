c3afff7d2e158bf0e019b34b202feb49ca702f07
tcrurup <tcrurup@gmail.com>
2022-07-12 16:06:17 -0600

    change homepage text

--- src/containers/Homepage.js
+++ src/containers/Homepage.js
@@ -1,7 +1,11 @@
 function Homepage(props){
 
     return <div id="homepage-container">
-        This is the homepage
+        Hello and welcome to my website.  This is an application I've built to both practice my programming and write my ideas.  The short term goal is to update this regularly to change to looks and add functionality with the long term goal being
+        to create a resource that teaches and shares ideas.
+
+        Please use the navbar above to check out what's going on
+         
     </div>
 }
 
