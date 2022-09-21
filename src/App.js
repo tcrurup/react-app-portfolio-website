18a96464ca3e2ace00ba1e621f21c19b5b04623d
tcrurup <49696152+tcrurup@users.noreply.github.com>
2022-09-20 23:05:34 -0600

    remove imports that are no longer used

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Homepage from './containers/Homepage';
 import Blog from './containers/Blog';
 import Resume from './containers/Resume';
 import Contact from './containers/Contact';
-import Navbar from "./containers/Navbar";
 import { useState, useEffect } from "react"
-import { Routes, Route, Outlet } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import CreateBlogPost from './components/CreateBlogPost';
 import PostContainer from './containers/PostContainer';
 import Layout from './Layout'
@@ -23,12 +21,10 @@ function App() {
     }
   }, [])//Array left blank so useEffect only happens on mount
 
-  //NEED TO CLEAN UP THIS ROUTING ITS REALLY BAD
-  //Instead have a base layout as the index and nest in all the rest.
   return (<>
   <Routes>
     <Route element={<Layout /> } >
-      <Route element = {<Homepage/>} />
+      <Route index element = {<Homepage/>} />
       <Route path = "/blog" element = {<Blog accessToken={ token }/>} >
         <Route path="posts" element = { <PostContainer />} />
         <Route path="posts/new" element = {<CreateBlogPost/>} />
